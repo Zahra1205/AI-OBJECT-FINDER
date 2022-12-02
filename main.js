de0558d9e46372216623494d2bad037099d1e938
Zahra1205 <78679374+Zahra1205@users.noreply.github.com>
2022-12-02 16:44:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 status = "";
 
 function preload(){
-video = createVideo("video.mp4");
-video.hide();
+
 }
 function setup(){
 canvas = createCanvas(480, 380);
@@ -11,7 +10,7 @@ canvas.center();
 }
 
 function draw(){
-image(video, 0, 0, 480, 380);
+
 }
 function start(){
     objectDetection = ml5.objectDetector("cocossd", modelLoaded);
